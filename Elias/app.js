8de5a621ad3a07c75ce3be878a0cc3f0cbf73d2e
blelias <33020817+blelias@users.noreply.github.com>
2018-03-05 19:34:50 +0100

    innlogging og sidebytting

--- Elias/app.js
+++ Elias/app.js
@@ -1,8 +1,89 @@
-const os = require('os');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link, HashRouter, Switch, Route } from 'react-router-dom';
+import { customerService } from './services';
 
-console.log("console log test");
+class User {
+			constructor(username, password){
+				this.username = username;
+				this.password = password;
+			}
+		}
 
-// Demonstration of a function (os.freemem()) that is not available in a web browser
-setInterval(function() {
-  document.getElementById('freemem').textContent = os.freemem();
-}, 500);
+var userArr = [];
+
+var p = new User('elias','pass123');
+userArr.push(p);
+
+function login(){
+	var inpUser = document.getElementById('inpUser').value;
+	var inpPassword = document.getElementById('inpPassword').value;
+
+	for(var i=0;i<userArr.length;i++){
+		if(inpUser==userArr[i].username&&inpPassword==userArr[i].password){
+		output.innerText = 'logget inn';
+		console.log('logget inn');
+		return true;
+	}else{
+		output.innerText = 'feil brukernavn/passord';
+		console.log('feil brukernavn/passord');
+		return false;
+		i++;
+		}
+	}
+}
+
+class Navbar extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1>Navigasjonsbaren</h1>
+				<Link to='/loginPage'>Logg ut</Link><br />
+				<Link to='/home'>Hjemmeside</Link>
+			</div>
+		);
+	}
+}
+
+class LoginPage extends React.Component {
+	render(){
+		return (
+			<div>
+				<h1>Innlogging</h1>
+				<input type='text' id='inpUser' placeholder='username' />
+				<input type='password' id='inpPassword' placeholder='password' /><span />
+				<button id='btn'>Login</button>
+				<div id='output'></div><br />
+				<Link to='/Home'>Hjemmeside</Link>
+			</div>
+		);
+	}
+	componentDidMount(){
+		document.getElementById('btn').onclick = () => {
+			login();
+		}
+		var output = document.getElementById('output');
+	}
+}
+
+class Home extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1>Hjemmeside</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render((
+  <HashRouter>
+    <div>
+      <Navbar />
+      <Switch>
+				<Route exact path='/home' component={Home}/>
+				<Route excat path='/loginPage' component={LoginPage}/>
+      </Switch>
+    </div>
+  </HashRouter>
+), document.getElementById('root'));
---
+++ Elias/services.js
@@ -0,0 +1,66 @@
+import mysql from 'mysql';
+
+// Setup database server reconnection when server timeouts connection:
+let connection;
+function connect() {
+  connection = mysql.createConnection({
+    host: 'mysql.stud.iie.ntnu.no',
+    user: 'eliasbj',
+    password: '1B3A3DMu',
+    database: 'eliasbj'
+  });
+
+  // Connect to MySQL-server
+  connection.connect((error) => {
+    if (error) throw error; // If error, show error in console and return from this function
+  });
+
+  // Add connection error handler
+  connection.on('error', (error) => {
+    if (error.code === 'PROTOCOL_CONNECTION_LOST') { // Reconnect if connection to server is lost
+      connect();
+    }
+    else {
+      throw error;
+    }
+  });
+}
+connect();
+
+// Class that performs database queries related to customers
+class CustomerService {
+  getCustomers(callback) {
+    connection.query('SELECT * FROM Customers', (error, result) => {
+      if (error) throw error;
+
+      callback(result);
+    });
+  }
+
+  getCustomer(id, callback) {
+    connection.query('SELECT * FROM Customers WHERE id=?', [id], (error, result) => {
+      if (error) throw error;
+
+      callback(result[0]);
+    });
+  }
+
+  addCustomer(firstName, city, callback) {
+    connection.query('INSERT INTO Customers (firstName, city) values (?, ?)', [firstName, city], (error, result) => {
+      if (error) throw error;
+
+      callback();
+    });
+  }
+
+  deleteCustomer(id){
+    connection.query('DELETE * FROM Customers WHERE i=?',[id],(error,result) => {
+      if(error) throw error;
+
+      callback(result[0]);
+    })
+  }
+}
+let customerService = new CustomerService();
+
+export { customerService };
